47d5030d6b2d719e5e997e64c28247f2a1926ab3
HeediBoy <shaheedmcpherson@gmail.com>
2021-11-23 08:06:55 +0200

    Created Map service and Crud Service

--- pet-clinic-data/src/main/java/services/VetService.java
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -2,10 +2,6 @@ package services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
 }
